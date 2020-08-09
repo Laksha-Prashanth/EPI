f43336c75b2b404d2ac6570aea6ab7dda803d153
Laksha-Prashanth <lakshaprashanth@gmail.com>
2020-08-09 00:48:15 -0400

    chapter 5 over

--- epi_judge_python/is_valid_sudoku.py
+++ epi_judge_python/is_valid_sudoku.py
@@ -2,10 +2,47 @@ from typing import List
 
 from test_framework import generic_test
 
+def box_check_helper(sudoku: List[List[int]], x, y) -> bool:
+    seen = set()
+    for i in range(3):
+        for j in range(3):
+            element = sudoku[i+x][j+y]
+            if element != 0:
+                if element in seen:
+                    return False
+                seen.add(sudoku[i+x][j+y])
+    return True
 
 # Check if a partially filled matrix has any conflicts.
-def is_valid_sudoku(partial_assignment: List[List[int]]) -> bool:
+def is_valid_sudoku(sudoku: List[List[int]]) -> bool:
     # TODO - you fill in here.
+
+    seen = set()
+
+    #row
+    for i in range(len(sudoku)):
+        for j in range(len(sudoku[i])):
+            if sudoku[i][j] != 0:
+                if sudoku[i][j] in seen:
+                    return False
+                seen.add(sudoku[i][j])
+        seen = set()
+    #column
+    seen = set()
+    for j in range(len(sudoku)):
+        for i in range(len(sudoku[j])):
+            if sudoku[i][j] != 0:
+                if sudoku[i][j] in seen:
+                    return False
+                seen.add(sudoku[i][j])
+        seen = set()
+    
+    #box
+    for i in range(0,9,3):
+        for j in range(0,9,3):
+            if not box_check_helper(sudoku, i,j):
+                    return False
+
     return True
 
 
--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -3,9 +3,35 @@ from typing import List
 from test_framework import generic_test
 
 
-def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+def matrix_in_spiral_order(matrix: List[List[int]]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    if not matrix:
+        return []
+    start = 0
+    result = []
+    size = len(matrix)*len(matrix[0])
+    while len(result) < size:
+
+        #->
+        for j in range(start,len(matrix[0])-start):
+            result.append(matrix[start][j])
+        if len(result) >= size:
+            break
+        for i in range(start+1,len(matrix)-start):
+            result.append(matrix[i][len(matrix)-1-start])
+        if len(result) >= size:
+            break
+        for j in range(len(matrix[0])-2-start,start-1,-1):
+            result.append(matrix[len(matrix)-1-start][j])
+        if len(result) >= size:
+            break
+        for i in range(len(matrix)-2-start,start,-1):
+            result.append(matrix[i][start])
+        if len(result) >= size:
+            break
+        start += 1
+
+    return result
 
 
 if __name__ == '__main__':
