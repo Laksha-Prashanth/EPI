aac3ccaf8816ef2a26c75bfe935d759349af866e
Laksha-Prashanth <lakshaprashanth@gmail.com>
2020-08-30 23:13:24 -0400

    started with heaps

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,28 @@
 from typing import List
+import heapq
 
 from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
     # TODO - you fill in here.
-    return []
+    heap = []
+    heapq.heapify(heap)
+    for i,element in enumerate(sorted_arrays):
+        heapq.heappush(heap, [element[0],i,0])
+    
+    result = []
+    empty = 0
+    while empty < len(sorted_arrays):
+        t = heapq.heappop(heap)
+        result.append(t[0])
+        if t[2] < len(sorted_arrays[t[1]])-1:
+            a = sorted_arrays[t[1]][t[2]+1]
+            heapq.heappush(heap,[a,t[1],t[2]+1])
+        else:
+            empty += 1
+    
+    return result
 
 
 if __name__ == '__main__':
