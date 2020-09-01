b4004a9c7f9c2ae803bb8eecc0e9787f737acbf5
Laksha-Prashanth <lakshaprashanth@gmail.com>
2020-09-01 00:56:09 -0400

    done with heaps

--- epi_judge_python/k_largest_in_heap.py
+++ epi_judge_python/k_largest_in_heap.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List
 
 from test_framework import generic_test, test_utils
@@ -5,7 +6,19 @@ from test_framework import generic_test, test_utils
 
 def k_largest_in_binary_heap(A: List[int], k: int) -> List[int]:
     # TODO - you fill in here.
-    return []
+    heap = []
+    heapq.heappush(heap, [-A[0], 0])
+    result = []
+    for _ in range(k):
+        curr = heapq.heappop(heap)
+        result.append(-curr[0])
+
+        if curr[1]*2+1 < len(A):
+            heapq.heappush(heap, [-A[curr[1]*2+1], curr[1]*2+1])
+        if curr[1]*2+2 < len(A):
+            heapq.heappush(heap, [-A[curr[1]*2+2], curr[1]*2+2])
+
+    return result
 
 
 if __name__ == '__main__':
--- epi_judge_python/online_median.py
+++ epi_judge_python/online_median.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import Iterator, List
 
 from test_framework import generic_test
@@ -5,7 +6,32 @@ from test_framework import generic_test
 
 def online_median(sequence: Iterator[int]) -> List[float]:
     # TODO - you fill in here.
-    return []
+    arr = list(sequence)
+    if not arr:
+        return []
+    left = [-arr[0]]
+    right = []
+    count = 1
+    result = [arr[0]]
+
+    for e in arr[1:]:
+        if e <= -left[0] or not right or e < right[0]:
+            heapq.heappush(left,-e)
+        else:
+            heapq.heappush(right,e)
+        count += 1
+        if len(right) > len(left):
+            heapq.heappush(left,-heapq.heappop(right))
+        elif len(left) > len(right) + 1:
+            heapq.heappush(right, -heapq.heappop(left))
+        
+        #median
+        if count % 2:
+            result.append(-left[0])
+        else:
+            result.append((-left[0]+right[0])/2)
+
+    return result
 
 
 def online_median_wrapper(sequence):
